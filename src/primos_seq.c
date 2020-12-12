acdca337fd1d0cb9ed361fc8d16b48fd03e74442
Gabriel <gabriel.silva@gmail.com>
2020-12-12 13:04:10 -0300

    Exemplos Sequenciais

--- src/primos_seq.c
+++ src/primos_seq.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-int primo (int n) {
-	int i;
+int primo (long int n) {
+	long int i;
        
 	for (i = 3; i < (int)(sqrt(n) + 1); i+=2) 
 	     if (n%i == 0) 
@@ -12,21 +12,20 @@ int primo (int n) {
 }
 
 int main(int argc, char *argv[]) { /* primos_seq.c  */
-	double t_inicial, t_final;
-	int cont = 0, total;
+	int total=0;
 	long int i, n;
 
 	if (argc < 2) {
-        printf("Valor inválido! Entre com o valor do maior inteiro\n");
-       	return 0;
-    } else {
-        n = strtol(argv[1], (char **) NULL, 10);
-    }
+            printf("Valor inválido! Entre com o valor do maior inteiro\n");
+       	    return 0;
+    	} else {
+            n = strtol(argv[1], (char **) NULL, 10);
+    	}
 
 	for (i = 3; i <= n; i += 2) 	
 	     if(primo(i) == 1) total++;
 		
-    total += 1;    /* Acrescenta o dois, que também é primo */
+    	total += 1;    /* Acrescenta o dois, que também é primo */
 	printf("Quant. de primos entre 1 e n: %d \n", total);
 	return(0);
 }
